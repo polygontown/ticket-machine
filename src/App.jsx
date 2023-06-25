4d86de68ba96b0086d6e9bab3d1cd2a75853c8a4
Devaraj P V <devarajperayil327@gmail.com>
2023-06-25 22:05:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <main>
+      <div className="headding">
+        <h2>Ticket application</h2>
+      </div>
+      <div className="button">
+        <button>Start trip</button>
+      </div>
+      Ticket app
+    </main>
+  );
+}
+
+export default App;
